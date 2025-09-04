0db61d9ed2eb56ea5826edb97af0a2de2a666687
Simbo-Fekadu <simbofekadu2629@gmail.com>
2025-09-04 13:13:45 +0300

    Restructure project: unify settings/urls, move listings app, add requirements

--- alx_travel_app/alx_travel_app/urls.py
+++ alx_travel_app/alx_travel_app/urls.py
@@ -15,8 +15,28 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+swagger_info = openapi.Info(
+    title="ALX Travel App API",
+    default_version='v1',
+    description="API documentation for ALX Travel App",
+    terms_of_service="https://www.example.com/terms/",
+    contact=openapi.Contact(email="support@example.com"),
+    license=openapi.License(name="MIT License"),
+)
+
+schema_view = get_schema_view(
+    swagger_info,
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('listings.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
